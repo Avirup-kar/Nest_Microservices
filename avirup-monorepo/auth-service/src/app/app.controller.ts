499473ca2d150a89d0fc6f4d94e9979405f65fb2
Avirup-kar <avirupk57@gmail.com>
2026-07-31 00:58:23 +0530

    creat controller and setup app.module

--- avirup-monorepo/auth-service/src/app/app.controller.ts
+++ avirup-monorepo/auth-service/src/app/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getData() {
-    return this.appService.getData();
+  @MessagePattern({ cmd: 'validate_user' })
+  handleValidateUser(@Payload() data: any): any {
+    console.log('Received data:', data);
+    if(data.userId === "1"){
+      return { status: 'success', user: { id: 1 , name: 'John Doe' } };
+    }
+    return { status: 'error', message: 'User not found' };
   }
 }
