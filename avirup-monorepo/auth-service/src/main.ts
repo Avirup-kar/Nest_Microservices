2748da9d020158a62c3ccc1e77292edae48c175b
Avirup-kar <avirupk57@gmail.com>
2026-07-30 22:16:05 +0530

    create microservice main

--- avirup-monorepo/auth-service/src/main.ts
+++ avirup-monorepo/auth-service/src/main.ts
@@ -1,12 +1,22 @@
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
-    transport: Transport.TCP,
-  });
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        host: '127.0.0.1',
+        port: 5005,
+      },
+    },
+  );
+
+  app.listen();
+
+  console.log('Auth microservice is running on port 5005');
 }
 
 bootstrap();
