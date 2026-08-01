b7e4552081990cd4d0bcfc6d909d17baf0b2c2a9
Avirup Kar <avirupk57@gmail.com>
2026-08-01 15:32:51 +0000

    create inventory-service

--- avirup-grpc/inventory-service/src/app/app.controller.ts
+++ avirup-grpc/inventory-service/src/app/app.controller.ts
@@ -1,12 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { GrpcMethod } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getData() {
-    return this.appService.getData();
-  }
+  @GrpcMethod('InventoryService', 'CheckStock') 
 }
--- avirup-grpc/inventory-service/src/main.ts
+++ avirup-grpc/inventory-service/src/main.ts
@@ -1,20 +1,20 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.GRPC,
+    options: {
+      package: 'inventory',
+      protoPath: join(process.cwd(), 'src/proto/inventory.proto'),
+      url: 'localhost:50051',
+    },
+  });
+  await app.listen();
+  console.log(
+    `🚀 GRPS service running on: http://localhost:50051`,
   );
 }
 
