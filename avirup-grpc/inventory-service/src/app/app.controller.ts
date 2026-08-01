45e9b0c2e6dc6c4d0eededc8c71c5b776aa58b99
Avirup Kar <avirupk57@gmail.com>
2026-08-01 16:10:59 +0000

    add again

--- avirup-grpc/inventory-service/src/app/app.controller.ts
+++ avirup-grpc/inventory-service/src/app/app.controller.ts
@@ -4,5 +4,18 @@ import { GrpcMethod } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
-  @GrpcMethod('InventoryService', 'CheckStock') 
+  @GrpcMethod('InventoryService', 'CheckStock')
+  checkStock(data: { productId: string }) {
+    const items: Record<string, number> = {
+      '123': 50,
+      '456': 0 
+    }
+     
+    const qty = items[data.productId] || 0;
+    
+    return { 
+      inStock: qty > 0, 
+      availableQuantity: qty 
+    };
+  }
 }
