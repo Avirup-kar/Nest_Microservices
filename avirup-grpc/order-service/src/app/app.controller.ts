2badf6c0904d090ac64aea5d7d3d77ba16e35235
Avirup Kar <avirupk57@gmail.com>
2026-08-01 19:29:56 +0000

    add again

--- avirup-grpc/order-service/src/app/app.controller.ts
+++ avirup-grpc/order-service/src/app/app.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, OnModuleInit } from '@nestjs/common';
+import { Controller, Get, Inject, OnModuleInit, Query } from '@nestjs/common';
 import { AppService } from './app.service';
-import { Observable } from 'rxjs';
+import { lastValueFrom, Observable } from 'rxjs';
+import { ClientGrpc } from '@nestjs/microservices';
 
 interface InventoryService {
   CheckStock(data: { productId: string }): Observable<{ inStock: boolean; availableQuantity: number }>;
@@ -9,4 +10,15 @@ interface InventoryService {
 @Controller()
 export class AppController implements OnModuleInit {
   private inventoryService!: InventoryService;
+
+  constructor(@Inject('INVENTORY_PACKAGE') private client: ClientGrpc) {}
+
+  onModuleInit() {
+    this.client.getService<InventoryService>('InventoryService');
+  }
+
+  @Get('check-item')
+  async bcheckItem(@Query('pid') pid: string) {
+     const stockStatus = await lastValueFrom(this.inventoryService.CheckStock({ productId: pid }));
+  }
 }
