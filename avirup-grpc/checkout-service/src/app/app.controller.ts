d4bfd14a74666827a9d77a170d031b086e51156a
Avirup Kar <avirupk57@gmail.com>
2026-08-03 16:19:38 +0000

    create the controller

--- avirup-grpc/checkout-service/src/app/app.controller.ts
+++ avirup-grpc/checkout-service/src/app/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getData() {
-    return this.appService.getData();
+  async processCheckout() {
+    return this.appService.discoverAndCallPayment();
   }
 }
