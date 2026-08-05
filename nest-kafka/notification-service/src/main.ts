486778a04a8ad7a14bb55d8eceb1c6d3953e723f
Avirup Kar <avirupk57@gmail.com>
2026-08-05 20:02:09 +0000

    craet notification-service server

--- nest-kafka/notification-service/src/main.ts
+++ nest-kafka/notification-service/src/main.ts
@@ -1,19 +1,26 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { MicroserviceOptions, Transport } from "@nestjs/microservices";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        clientId: 'notification-consumer',
+        brokers: ['localhost:9092'],
+      },
+      consumer: {
+        groupId: 'notification-group',
+      },
+      subscribe: {
+        fromBeginning: true,
+      }
+    },
+  });
+  
+  await app.listen();
+  console.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
   );
 }
