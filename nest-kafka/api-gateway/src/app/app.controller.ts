bf5eda86d6d4e1a37bd01b61be5c187dcf073bc5
Avirup Kar <avirupk57@gmail.com>
2026-08-07 23:03:34 +0530

    add again

--- nest-kafka/api-gateway/src/app/app.controller.ts
+++ nest-kafka/api-gateway/src/app/app.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, OnModuleInit, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ClientKafka } from '@nestjs/microservices';
+
 
 @Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
+export class AppController implements OnModuleInit {
+  constructor(@Inject('KAFKA_SERVICE') private readonly kafkaClient: ClientKafka) {}
 
-  @Get()
-  getData() {
-    return this.appService.getData();
+  async onModuleInit() {
+    // Subscribe to the response topic for the 'test' message
+    this.kafkaClient.connect();
+  }
+
+  @Get('create-user')
+  async createUser() {
+    const user = {
+      id: 1,
+      name: 'John Doe',
+      email: 'john.doe@example.com'
+    };
+
+    // Send the 'create-user' message to the Kafka topic
+    this.kafkaClient.emit('user_created', user);
+    return { message: 'User creation event sent to Kafka', user };
   }
 }
--- nest-kafka/notification-service/src/app/app.controller.ts
+++ nest-kafka/notification-service/src/app/app.controller.ts
@@ -4,7 +4,7 @@ import { EventPattern, Payload } from '@nestjs/microservices';
 @Controller()
 export class AppController {
   @EventPattern('user_created')
-  async handleUserCreated(@Payload() data:  any) {
+  async handleUserCreated(@Payload() data: any) {
     console.log('Received user_created event');
     console.log('Data:', data);
   }
