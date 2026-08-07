3660ae2e497e8907ed120f7a7cf1f6beb477bef6
Avirup Kar <avirupk57@gmail.com>
2026-08-07 23:10:24 +0530

    add again

--- nest-kafka/notification-service/src/app/app.controller.ts
+++ nest-kafka/notification-service/src/app/app.controller.ts
@@ -1,16 +1,26 @@
 import { Controller, OnModuleInit, Inject } from '@nestjs/common';
-import { EventPattern, Payload } from '@nestjs/microservices';
+import { EventPattern, Payload, ClientKafka } from '@nestjs/microservices';
 
 @Controller()
 export class AppController implements OnModuleInit {
+  constructor(@Inject('KAFKA_CLIENT') private readonly kafkaClient: ClientKafka) {}
+
+  
+  async onModuleInit() {
+    await this.kafkaClient.connect();
+    console.log('Kafka client connected and ready to receive messages');
+  }
+  
   @EventPattern('user_created')
   async handleUserCreated(@Payload() data: any) {
-    console.log('Received user_created event');
-    console.log('Data:', data);
+    try {
+      console.log('Received user_created event:', data);
+      console.log('Data', data);
+      throw new Error('Notification service failed'); // Simulate an error for testing
+    } catch (error) {
+      console.error('Error processing user_created event:', error);
+    }
   }
 
-  async onModuleInit() {
-    console.log('AppController initialized');
-  }
 }
 
