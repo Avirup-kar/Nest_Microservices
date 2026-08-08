84d2067684f5e1ed8cdd65c74b201543fe05a582
Avirup Kar <avirupk57@gmail.com>
2026-08-09 01:03:05 +0530

    create again

--- avirup-cache/product-service/src/app/app.module.ts
+++ avirup-cache/product-service/src/app/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CacheModule } from '@nestjs/cache-manager';
+import { redisStore } from 'cache-manager-redis-store';
 
 @Module({
-  imports: [],
+  imports: [
+    CacheModule.registerAsync({
+      useFactory: async () => ({
+        store: await redisStore({
+           socket: {
+            host: 'localhost',
+            port: 6379,
+          },
+          ttl: 60000, // 1 minute
+        })
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ avirup-cache/product-service/src/app/fack-db.ts
@@ -0,0 +1,12 @@
+export const products = [
+  {
+    id: 1,
+    name: 'Macbook Pro',
+    price: 2500
+  },
+  {
+    id: 2,
+    name: 'iphone 17',
+    price: 2000
+  }
+]
