f317c4a7db7e928c221fb755c7c5b2672c0639aa
Avirup Kar <avirupk57@gmail.com>
2026-08-09 01:26:24 +0530

    create again

--- avirup-cache/product-service/src/app/app.service.ts
+++ avirup-cache/product-service/src/app/app.service.ts
@@ -1,6 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { CACHE_MANAGER } from "@nestjs/cache-manager";
+import { Injectable, Inject } from '@nestjs/common';
+import type { Cache } from 'cache-manager';
+import{ products } from './fack-db';
+ 
 
 @Injectable()
 export class AppService {
-  
+  constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {}
+
+  async getProducts(id: number) {
+     const cacheKey = `product:${id}`;
+    const cachedProduct = await this.cacheManager.get(cacheKey);
+
+    if (cachedProduct) {
+      console.log('cache hit');
+      return cachedProduct;
+    }
+
+    const product = products.find((p) => p.id === id);
+    if(product) {
+      await this.cacheManager.set(cacheKey, product);
+      return product;
+    }
+    console.log('cache miss');
+  }
 }
