f5d13d771d6ec021af39f7885938d3cb84c313d2
Avirup Kar <avirupk57@gmail.com>
2026-08-10 00:47:58 +0530

    create redis servic

--- avirup-cache/product-service/guards/rate-limit.guard.ts
+++ avirup-cache/product-service/guards/rate-limit.guard.ts
@@ -1,2 +1,8 @@
 import { CanActivate, ExecutionContext, Injectable, BadRequestException } from "@nestjs/common";
 import { RedisService } from "../redis/redis.service";
+
+
+@Injectable()
+export class RateLimitGuard implements CanActivate {
+    constructor(private readonly redisService: RedisService) {}
+}
