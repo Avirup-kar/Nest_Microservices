d485e8ad658de48441cfe5075871c32c3c926a37
Avirup Kar <avirupk57@gmail.com>
2026-08-09 22:50:42 +0530

    create redis servic

---
+++ avirup-cache/product-service/redis/redis.service.ts
@@ -0,0 +1,13 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { createClient } from 'redis';
+
+@Injectable()
+export class RedisService implements OnModuleInit {
+    public client = createClient({
+       url: 'redis://localhost:6379',
+    });
+
+    async onModuleInit() {
+        await this.client.connect();
+    }
+}
