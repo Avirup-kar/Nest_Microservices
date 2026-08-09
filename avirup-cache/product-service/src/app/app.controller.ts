b7a5936c5be9fdd82758797af8da7fdc3c301824
Avirup Kar <avirupk57@gmail.com>
2026-08-10 01:30:06 +0530

    create redis servic

--- avirup-cache/product-service/src/app/app.controller.ts
+++ avirup-cache/product-service/src/app/app.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, Param, Patch, UseGuards, } from '@nestjs/common';
 import { AppService } from './app.service';
-// import { RateLimitGuard } from '../../guards/rate-limit.guard';
+import { RateLimitGuard } from '../guards/rate-limit.guard'
 
 @Controller()
 export class AppController {
@@ -18,4 +18,14 @@ export class AppController {
 
   @Get('products')
   @UseGuards(RateLimitGuard)
+  getAllProducts() {
+    return {
+      success: true,
+      products: [
+        { id: 1, name: 'Product 1', price: 10 },
+        { id: 2, name: 'Product 2', price: 20 },
+        { id: 3, name: 'Product 3', price: 30 },
+      ],
+    };
+  }
 }
