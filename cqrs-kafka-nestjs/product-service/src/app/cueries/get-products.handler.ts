ce98c07eff7384a0fa905657435ba1ce63e48da7
Avirup Kar <avirupk57@gmail.com>
2026-08-11 02:11:21 +0530

    fix somthing

--- cqrs-kafka-nestjs/product-service/src/app/cueries/get-products.handler.ts
+++ cqrs-kafka-nestjs/product-service/src/app/cueries/get-products.handler.ts
@@ -1,8 +1,8 @@
-import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { QueryHandler, ICommandHandler } from '@nestjs/cqrs';
 import { GetProductsQuery } from './get-products.cueries';
 import { readDb } from '../product.store';
 
-@CommandHandler(GetProductsQuery)
+@QueryHandler(GetProductsQuery)
 export class GetProductsHandler implements ICommandHandler<GetProductsQuery> {
-    
+
 }
