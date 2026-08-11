1686b69dc87896ffc6167e0d7cd94ff3c54c7a9e
Avirup Kar <avirupk57@gmail.com>
2026-08-11 21:24:54 +0530

    set the kafta in modue

--- cqrs-kafka-nestjs/product-service/src/app/app.module.ts
+++ cqrs-kafka-nestjs/product-service/src/app/app.module.ts
@@ -1,10 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CqrsModule } from '@nestjs/cqrs';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ProductConsumer } from './product.consumer';
+import { ProductController } from './product.controller';
+import { GetProductsHandler } from './queries/get-products.handler';
+import { CreateProductHandler } from './commands/create-product.handler';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    CqrsModule,
+    ClientsModule.register([
+      {
+        name: 'KAFKA_SERVICE',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            clientId: 'cqrs-service',
+            brokers: ['localhost:9092'],
+          },
+          consumer: {
+            groupId: 'cqrs-group',
+          },
+        },
+      },
+    ]),
+  ],
+  controllers: [ProductController, ProductConsumer],
+  providers: [CreateProductHandler, GetProductsHandler],
 })
 export class AppModule {}
