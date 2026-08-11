193e13241301c72498d80de4587c1aee50a2afaf
Avirup Kar <avirupk57@gmail.com>
2026-08-11 20:55:00 +0530

    add again

--- cqrs-kafka-nestjs/product-service/src/app/product.controller.ts
+++ cqrs-kafka-nestjs/product-service/src/app/product.controller.ts
@@ -1,20 +1,27 @@
-import { Body, Controller, Get, Post, Inject } from "@nestjs/common";
-import { CommandBus, QueryBus } from "@nestjs/cqrs";
-import { ClientKafka } from "@nestjs/microservices";
-import { createProductCommand } from "./commands/create-product.command";
-import { GetProductsQuery } from "./queries/get-products.queries";
+import { Body, Controller, Get, Post, Inject } from '@nestjs/common';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
+import { ClientKafka } from '@nestjs/microservices';
+import { createProductCommand } from './commands/create-product.command';
+import { GetProductsQuery } from './queries/get-products.queries';
 
 @Controller('products')
 export class ProductController {
-    constructor(
-        private readonly commandBus: CommandBus,
-        private readonly queryBus: QueryBus,
-        @Inject('KAFKA_SERVICE') private readonly kafkaClient: ClientKafka
-    ) {}
+  constructor(
+    private readonly commandBus: CommandBus,
+    private readonly queryBus: QueryBus,
+    @Inject('KAFKA_SERVICE') private readonly kafkaClient: ClientKafka,
+  ) {}
 
-    @Post()
-    async create(@Body() body: any) {
-        const product = await this.commandBus.execute(new createProductCommand(body.name));
-        this.kafkaClient.emit('product_created', JSON.stringify(product));
-    }
+  @Post()
+  async create(@Body() body: any) {
+    const product = await this.commandBus.execute(
+      new createProductCommand(body.name),
+    );
+    this.kafkaClient.emit('product_created', JSON.stringify(product));
+
+    return {
+      message: 'Command Executed!',
+      product,
+    };
+  }
 }
