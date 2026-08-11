3605f163c0f0e2e565fa2b24f4723001fa8b9e84
Avirup Kar <avirupk57@gmail.com>
2026-08-11 21:26:05 +0530

    set the kafta in modue

--- cqrs-kafka-nestjs/product-service/src/app/app.module.ts
+++ cqrs-kafka-nestjs/product-service/src/app/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CqrsModule } from '@nestjs/cqrs';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { ProductConsumer } from './product.consumer';
