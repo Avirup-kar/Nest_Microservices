69375c7b670ce59d68a8536074547ec9933fb0cc
Avirup Kar <avirupk57@gmail.com>
2026-08-11 20:30:51 +0530

    add again

--- cqrs-kafka-nestjs/product-service/src/app/product.controller.ts
+++ cqrs-kafka-nestjs/product-service/src/app/product.controller.ts
@@ -1,10 +1,19 @@
 import { Body, Controller, Get, Post, Inject } from "@nestjs/common";
 import { CommandBus, QueryBus } from "@nestjs/cqrs";
 import { ClientKafka } from "@nestjs/microservices";
-import { CreateProductCommand } from "./commands/create-product.command";
+import { createProductCommand } from "./commands/create-product.command";
 import { GetProductsQuery } from "./queries/get-products.queries";
 
 @Controller('products')
 export class ProductController {
+    constructor(
+        private readonly commandBus: CommandBus,
+        private readonly queryBus: QueryBus,
+        @Inject('KAFKA_SERVICE') private readonly kafkaClient: ClientKafka
+    ) {}
 
+    @Post()
+    async create(@Body() body: any) {
+        const product = await this.commandBus.execute(new createProductCommand(body.name));
+    }
 }
