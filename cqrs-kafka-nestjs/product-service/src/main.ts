66c612e200a51af0bf1cc9a8aa359ee4fee37bd0
Avirup Kar <avirupk57@gmail.com>
2026-08-11 21:29:12 +0530

    set the kafta in modue

--- cqrs-kafka-nestjs/product-service/src/main.ts
+++ cqrs-kafka-nestjs/product-service/src/main.ts
@@ -1,20 +1,26 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
+  app.connectMicroservice({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        clientId: 'cqrs-consumer',
+        brokers: ['localhost:9092'],
+      },
+      consumer: {
+        groupId: 'cqrs-group',
+      }
+    }
+  });
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
+    `🚀 Application is running on: http://localhost:${port}`,
   );
 }
 
