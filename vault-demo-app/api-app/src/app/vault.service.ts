68b4fb310ff1ace732038cbea1294050562206c4
Avirup Kar <avirupk57@gmail.com>
2026-08-16 00:52:30 +0530

    creat ethe vault service

--- vault-demo-app/api-app/src/app/vault.service.ts
+++ vault-demo-app/api-app/src/app/vault.service.ts
@@ -13,4 +13,19 @@ export class vaultService implements OnModuleInit {
             token: 'my-super-secret-token'
         });
     }
+
+    async onModuleInit() {
+        try {
+            const response: any = await this.vault.read('secret/data/secret/data/config');
+
+            if(response?.data?.data) {
+                this.dbSecret = response.data.data.DATABASE_URL;
+                console.log('Database URL retrieved from Vault:', this.dbSecret);
+            } else {
+                console.error('No data found in the Vault response.');
+            }
+        } catch (error) {
+            console.error('Error occurred while fetching data from Vault:', error);
+        }
+    }
 }
