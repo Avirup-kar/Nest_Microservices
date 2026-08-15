302586c09c2dd00a79a61cec48b1126b12726e38
Avirup Kar <avirupk57@gmail.com>
2026-08-16 00:55:07 +0530

    add at module

--- vault-demo-app/api-app/src/app/app.module.ts
+++ vault-demo-app/api-app/src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { vaultService } from './vault.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [vaultService],
 })
 export class AppModule {}
