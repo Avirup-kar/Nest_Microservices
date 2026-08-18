aa06cff886b5478af33532735e16dd4796589a16
Avirup Kar <avirupk57@gmail.com>
2026-08-18 19:58:37 +0530

    add again

--- resilience-demo/apps/payment/src/app/app.service.ts
+++ resilience-demo/apps/payment/src/app/app.service.ts
@@ -3,5 +3,11 @@ import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 @Injectable()
 export class AppService {
   private isServiceHealthy = true;
-  
+
+  processPayment(amount: number) {
+    if(!this.isServiceHealthy){
+      throw new HttpException('Payment gatway is down', HttpStatus.INTERNAL_SERVER_ERROR)
+    }
+    return 
+  }
 }
