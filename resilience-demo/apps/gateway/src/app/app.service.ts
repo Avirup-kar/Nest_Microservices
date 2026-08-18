5efc7b02c1e833dce31fff3fd9b9f8a6ec5947f5
Avirup Kar <avirupk57@gmail.com>
2026-08-19 01:36:42 +0530

    craeting teh srevice of payment

--- resilience-demo/apps/gateway/src/app/app.service.ts
+++ resilience-demo/apps/gateway/src/app/app.service.ts
@@ -1,11 +1,31 @@
 import { Injectable, Logger } from '@nestjs/common';
-import * as CircuitBreaker from 'opossum';
+import axios from 'axios';
+import CircuitBreaker from 'opossum';
 
 @Injectable()
 export class AppService {
  private readonly logger = new Logger(AppService.name);
- private readonly breaker = CircuitBreaker;
+ private readonly breaker: CircuitBreaker<[amount: number], any>;
  private readonly paymentServiceUrl = "http://localhost:3001/api/payment/process"
 
- 
+ constructor() {
+  const paymentAction = async (amount: number) {
+    return this.executeWithRetry(() => axios.post(this.paymentServiceUrl, {amount}), 3, 1000)
+  } 
+
+  const options: CircuitBreaker.Options = {
+    timeout: 3000,
+    errorThresholdPercentage: 50,
+    resetTimeout: 10000,
+  }
+  this.breaker = new CircuitBreaker(paymentAction, options);
+
+  this.breaker.fallback((amount: number) => {
+   this.logger.warn('Circuit is Open. Fallback Response!')
+   return {
+    status: 'fallback_active',
+    message:'Payment System is currently busy'
+   }
+  })
+ }
 }
