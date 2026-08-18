656f0ddb81c3cd430d893bd087a0b47df28a3015
Avirup Kar <avirupk57@gmail.com>
2026-08-18 22:23:59 +0530

    create controller for payment service

--- resilience-demo/apps/payment/src/app/app.controller.ts
+++ resilience-demo/apps/payment/src/app/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getData() {
-    return this.appService.getData();
+  @Post('process')
+  processPayment(@Body() body: {amout: number}) {
+    return this.appService.processPayment(body.amout)
+  }
+
+  @Get('toggle')
+  toggleHealth(@Query('status') status: string) {
+
   }
 }
