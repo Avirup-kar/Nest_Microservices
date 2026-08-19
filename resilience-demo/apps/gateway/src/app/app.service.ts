d6fec8441890cf421a02a568b48916318f892772
Avirup Kar <avirupk57@gmail.com>
2026-08-19 22:42:49 +0530

    create executeWithRetry

--- resilience-demo/apps/gateway/src/app/app.service.ts
+++ resilience-demo/apps/gateway/src/app/app.service.ts
@@ -4,37 +4,56 @@ import CircuitBreaker from 'opossum';
 
 @Injectable()
 export class AppService {
- private readonly logger = new Logger(AppService.name);
- private readonly breaker: CircuitBreaker;
- private readonly paymentServiceUrl = "http://localhost:3001/api/payment/process"
+  private readonly logger = new Logger(AppService.name);
+  private readonly breaker: CircuitBreaker;
+  private readonly paymentServiceUrl = 'http://localhost:3001/api/payment/process';
 
- constructor() {
-  const paymentAction = async (amount: number) {
-    return this.executeWithRetry(() => axios.post(this.paymentServiceUrl, {amount}), 3, 1000)
-  } 
+  constructor() {
+    const paymentAction = async (amount: number) => {
+      return this.executeWithRetry(
+        () => axios.post(this.paymentServiceUrl, { amount }),
+        3,
+        1000,
+      );
+    };
 
-  const options: CircuitBreaker.Options = {
-    timeout: 3000,
-    errorThresholdPercentage: 50,
-    resetTimeout: 10000,
+    const options: CircuitBreaker.Options = {
+      timeout: 3000,
+      errorThresholdPercentage: 50,
+      resetTimeout: 10000,
+    };
+    this.breaker = new CircuitBreaker(paymentAction, options);
+
+    this.breaker.fallback((amount: number) => {
+      this.logger.warn('Circuit is Open. Fallback Response!');
+      return {
+        status: 'fallback_active',
+        message: 'Payment System is currently busy',
+        amount,
+      };
+    });
+
+    this.breaker.on('open', () =>
+      this.logger.error('Circuit Breaker State: OPEN (Blocked)'),
+    );
+    this.breaker.on('close', () =>
+      this.logger.log('Circuit Breaker State: CLOSED(Normal)'),
+    );
+    this.breaker.on('halfOpen', () =>
+      this.logger.warn('Circuit Breaker State: HALF_OPEN(Testing)'),
+    );
   }
-  this.breaker = new CircuitBreaker(paymentAction, options);
 
-  this.breaker.fallback((amount: number) => {
-   this.logger.warn('Circuit is Open. Fallback Response!')
-   return {
-    status: 'fallback_active',
-    message:'Payment System is currently busy',
-    amount
-   }
-  })
-
-  this.breaker.on('open', () => this.logger.error('Circuit Breaker State: OPEN (Blocked)'));
-  this.breaker.on('close', () => this.logger.log('Circuit Breaker State: CLOSED(Normal)'))
-  this.breaker.on('halfOpen', () => this.logger.warn('Circuit Breaker State: HALF_OPEN(Testing)'))
- }
-
- private executeWithRetry(
-  fn: () => Promise<any>
- )
+  private async executeWithRetry(
+    fn: () => Promise<any>,
+    retries: number,
+    delay: number,
+  ): Promise<any> {
+    try {
+      const responce = await fn();
+      return responce.data;
+    } catch (error) {
+      
+    }
+  }
 }
